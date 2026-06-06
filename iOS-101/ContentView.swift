0b59c3917a627b992b19eb3e562a9983fe5a1c2d
dinan <dinandilmith58@gmail.com>
2026-06-06 14:39:12 +0530

    Initial Commit

---
+++ iOS-101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-101
+//
+//  Created by Student1 on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-101/iOS_101App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_101App.swift
+//  iOS-101
+//
+//  Created by Student1 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iOS_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
